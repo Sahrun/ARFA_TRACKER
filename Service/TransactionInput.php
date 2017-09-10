0733fc5db7edafe2a7e930e5f5507a047e302aa7
Sahrun <sahrun.raja.tega@gmail.com>
2017-09-10 20:18:43 +0700

    Update Queue Transaction

--- Service/TransactionInput.php
+++ Service/TransactionInput.php
@@ -18,6 +18,10 @@ switch ($request) {
     case 'getPackage':
         GetPackage($connection);
         break;
+    case 'gatDataTransctionOrder':
+        GetDataTransctionOrder($connection);
+        break;
+        
     default:
      echo "service Not Found";
 }
@@ -51,5 +55,36 @@ if ($result) {
 		echo "Terjadi Kesalahan" . mysqli_error($connection);
 		}
     }
-
+function GetDataTransctionOrder($connection){
+$GetTransaction="SELECT o.id_order,
+					o.name,
+					o.email ,
+					o.no_telpon ,
+					p.package_name,
+					o.number_people,
+					o.pickup_location,
+					o.date_pickup,
+					o.date_order 
+            FROM order_package o 
+            INNER JOIN package p
+            ON o.id_package = p.id_package";
+$result = mysqli_query($connection, $GetTransaction);
+if ($result) {
+	     $data = array();
+	     while ($rw = mysqli_fetch_array($result)) {
+	     	$data[] = array('id_order' => $rw['id_order'],
+	     		            'name'=>$rw['name'],
+	     		            'email'=>$rw['email'],
+	     		            'no_telpon'=>$rw['no_telpon'],
+	     		            'package_name'=>$rw['package_name'],
+	     		            'number_people'=>$rw['number_people'],
+	     		            'pickup_location'=>$rw['pickup_location'],
+	     		            'date_pickup'=>$rw['date_pickup'],
+	     		            'date_order'=>$rw['date_order'],);
+	     }
+	     echo json_encode($data);
+		} else {
+		echo "Terjadi Kesalahan" . mysqli_error($connection);
+		}
+    }
 ?>
